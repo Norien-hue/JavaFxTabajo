4bc25871db756cc19f5660f834a48893964005d8
Norien <jsanmar937@g.educaand.es>
2025-06-09 13:19:53 +0200

    Todo subido

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
+}
+
+group = "org.example"
+version = "1.0"
+
+application{
+    mainClass="dam.tema8.ejercicio6.kiosko.modelo.Programa"
+}
+
+javafx{
+    modules("javafx.controls","javafx.fxml")
+    version="21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
