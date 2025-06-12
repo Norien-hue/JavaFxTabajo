8cdc6d06bde3290af700c65e807993c3e56135d5
Norien <jsanmar937@g.educaand.es>
2025-06-12 11:38:53 +0200

    Cambiado el build por que Juan Diego me dijo como podia solucionar lo de que los tests no se pudieran leer

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,3 @@ javafx{
 repositories {
     mavenCentral()
 }
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
--- src/test/java/com/programacion/paint/clases/PuntoTest.java
+++ src/test/java/com/programacion/paint/clases/PuntoTest.java
@@ -1,6 +1,26 @@
 package com.programacion.paint.clases;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
+
 public class PuntoTest {
-  
+    @Test(expected = IllegalArgumentException.class)
+    public void test1(){
+        Punto p = new Punto(-3,25);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void test2(){
+        Punto p = new Punto(3,-25);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void test3(){
+        Punto p = new Punto(-3,-25);
+    }
+    @Test
+    public void test4(){
+        Punto p = new Punto(3,25);
+        assertEquals(3, p.x());
+        assertEquals(25, p.y());
+    }
 }
