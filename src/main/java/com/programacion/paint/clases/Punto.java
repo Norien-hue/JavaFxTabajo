ef51ee753307824f3ce0aeb6c5452b2dfd103ad5
Norien <jsanmar937@g.educaand.es>
2025-06-12 11:57:50 +0200

    Añadidas de vuelta la clase Punto y TipoPincel (le quite un error)

--- src/main/java/com/programacion/paint/clases/Punto.java
+++ src/main/java/com/programacion/paint/clases/Punto.java
@@ -2,6 +2,8 @@ package com.programacion.paint.clases;
 
 public record Punto(double x, double y) {
     public Punto{
-
+        if(x<0 || y<0){
+            throw new IllegalArgumentException("Los parametros deben ser mayor o iguales a cero");
+        }
     }
 }
--- src/main/java/com/programacion/paint/clases/TipoPincel.java
+++ src/main/java/com/programacion/paint/clases/TipoPincel.java
@@ -6,7 +6,7 @@ public enum TipoPincel {
 
     PINCEL_BASICO(new PincelBasico()),
     PINCEL_GORDO(new PincelGordo()),
-    PINCEL_GROSOR_VARIABLE(new PincelGrosorVariable()),
+    PINCEL_GROSOR_VARIABLE(new PincelGrosorVariable(2, 20)),
     PINCEL_CONTINUO(new PincelContinuo()),
     PINCEL_RECTANGULO(new PincelRectangulo()),
     PINCEL_ESTRELLA(new PincelEstrella());
