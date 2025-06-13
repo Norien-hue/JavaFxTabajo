7b7f2359f889a8bd2edcdc15cb600e27a79d67d3
Samuel Pozo <pozosamuel4@gmail.com>
2025-06-13 04:08:01 +0200

    Arreglos varios

--- src/main/java/com/programacion/paint/clases/PincelCirculo.java
+++ src/main/java/com/programacion/paint/clases/PincelCirculo.java
@@ -5,6 +5,7 @@ import javafx.scene.canvas.GraphicsContext;
 public interface PincelCirculo extends Pincel{
     public int getRadio();
     public default void dibujar(GraphicsContext g, Punto p) {
-        this.dibujar(g, p);
+        double radio = getRadio();
+        g.fillOval(p.x() - radio, p.y() - radio, radio*2, radio*2);
     }
 }
--- src/main/java/com/programacion/paint/clases/PincelEstrella.java
+++ src/main/java/com/programacion/paint/clases/PincelEstrella.java
@@ -3,15 +3,22 @@ package com.programacion.paint.clases;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+import java.io.InputStream;
+
 public class PincelEstrella implements Pincel{
-    private static final Image ESTRELLA = null;
+    private static final Image ESTRELLA = new Image(ClassLoader.getSystemResourceAsStream("estrella.png"));
 
     @Override
     public void dibujar(GraphicsContext g, Punto p) {
-        throw new UnsupportedOperationException("no programado");
+        g.drawImage(ESTRELLA, p.x(), p.y());
     }
 
-    PincelEstrella(){
-        throw new UnsupportedOperationException("no programado");
+    PincelEstrella() {
+    }
+
+    @Override
+    public String toString() {
+        return "Pincel estrella";
     }
 }
+
