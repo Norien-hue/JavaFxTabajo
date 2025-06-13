ebb6a66f17dc7b9a4489c09c1c1209068827324d
Norien <jsanmar937@g.educaand.es>
2025-06-13 10:32:39 +0200

    Puesto el programa a cargar la ventana como lo hemos hecho en el tema

--- src/main/java/com/programacion/paint/Programa.java
+++ src/main/java/com/programacion/paint/Programa.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Programa extends Application {
 
     public static void main(String[] args) {
@@ -12,12 +14,29 @@ public class Programa extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/java/resources/frmPaint.fxml"));
-        Scene scene = new Scene(loader.load());
+    public void start(Stage ventana){
+        FXMLLoader loader = new FXMLLoader();
+        try{
+            Scene scene = new Scene(loader.load(ClassLoader.getSystemResourceAsStream("frmPaint.fxml")));
 
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
+            ventana.setScene(scene);
+            ventana.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
+
+    /*
+    getClass().getResource("/java/resources/frmPaint.fxml")
+
+    FXMLLoader loader = new FXMLLoader();
+        try{
+            Scene escena = new Scene(loader.load(ClassLoader.getSystemResourceAsStream("frmRobot.fxml")));
+            ventana.setScene(escena);
+            ventana.show();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+     */
 }
